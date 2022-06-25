69663d97efd56b1f251d34dea55259e7c975f527
Daler <dsarifkulo021@gmail.com>
2022-06-25 17:41:57 +0500

    fix problems

---
+++ index.js
@@ -0,0 +1,27 @@
+// // method number // github
+
+// // let a = "hello"
+// let a = 10
+// let b = 20
+
+// // a = a.toString()
+
+// console.log(
+//     // Math.floor(a) // down
+//     // Math.ceil(a) // up
+//     // Math.round(a) // logical
+
+//     // 
+//     // parseInt(a) // перевод из  строки в число и делает целым
+//     // parseFloat(a) // // перевод из  строки в число 
+//     // +a // перевод из  строки в число 
+
+//     // Math.max(a, b)
+//     // Math.min(a, b, 28, -19)
+//     // Math.pow(7,2) // -> **
+
+//     // Math.floor(Math.random() * 1000)
+// );
+
+// GITHUB
+
