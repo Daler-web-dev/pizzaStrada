2a997ba75594345a4e702cd287f0472fed827e76
Daler <dsarifkulo021@gmail.com>
2022-06-30 18:02:41 +0500

    fix

--- index.js
+++ index.js
@@ -1,27 +1,93 @@
-// // method number // github
+// let a = prompt('wraite ur name').toLowerCase().trim()
 
-// // let a = "hello"
-// let a = 10
-// let b = 20
+// if(a === 'alex') {
+//     console.log('Hello ' + a);
+// } else {
+//     console.log('sorry mr ' + a);
+// }
 
-// // a = a.toString()
+// let num = prompt('number')
 
-// console.log(
-//     // Math.floor(a) // down
-//     // Math.ceil(a) // up
-//     // Math.round(a) // logical
+// console.log(num % 2 === 0);
 
-//     // 
-//     // parseInt(a) // перевод из  строки в число и делает целым
-//     // parseFloat(a) // // перевод из  строки в число 
-//     // +a // перевод из  строки в число 
+// if(num % 2 === 0) {
+//     console.log('even');
+// } else {
+//     console.log('odd');
+// }
 
-//     // Math.max(a, b)
-//     // Math.min(a, b, 28, -19)
-//     // Math.pow(7,2) // -> **
+// let name = 'zoir'
+// let age = 19
 
-//     // Math.floor(Math.random() * 1000)
-// );
+// if(name === 'alex' && age >= 18) {
+//     console.log('welcome bratishka');
+// } else {
+//     console.log('uyga bor');
+// }
 
-// GITHUB
 
+
+// let guests_list = 'zoir timur diyor shaxzod muxammad jonibek'
+
+
+
+// if(guests_list.includes(name)) {
+//     alert('welcome')
+// } else {
+//     alert('go home')
+// }
+
+// let name = prompt('what is your name?') // muxammad
+
+// if(name == 'zoir') {
+//     alert('Salam zoirka')
+// } else if(name === 'timur') {
+//     alert('Hi Tima')
+// } else if(name === 'diyor') {
+//     alert('Diyorka kak vi')
+// } else {
+//     alert('Вы кто ?')
+// }
+
+// let a = +prompt('a')
+// let m = prompt('+ - * /')
+// let b = +prompt('b')
+
+
+// if(m === "+") {
+//     console.log(a + b);
+// } else if(m === "-") {
+//     console.log(a - b);
+// }  else if(m === "*") {
+//     console.log(a * b);
+// }  else if(m === "/") {
+//     console.log(a / b);
+// }
+
+// switch (m) {
+//     case "+":
+//         console.log(a + b);
+//         break;
+//     case "-":
+//         console.log(a - b);
+//         break;
+//     case "*":
+//         console.log(a * b);
+//         break;
+//     case "/":
+//         console.log(a / b);
+//         break;
+//     default:
+//         break;
+// }
+
+
+let day = prompt('') // 
+
+// должен сказать какая сейчас декада 
+
+
+let month = prompt('month')
+
+
+// число месяца и код должен какой сейчас сезон
